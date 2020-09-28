3dc4cbfc6ec7aaee7217fe254b7340f4dca5fd0a
j-frilot <frilotdr@gmail.com>
2020-09-28 10:55:08 -0500

    first commit

---
+++ rock.js
@@ -0,0 +1,18 @@
+var rock = rock
+var paper = paper
+var scissor = scissor
+
+var user = scissor
+var computer = rock
+
+if (user == rock && computer == scissor) {
+    console.log('user wins')
+} else if (user == scissor && computer == papper) {
+    console.log('user wins');
+} else if (user == papper && computer == rock) {
+    console.log('user wins');
+} else {
+    console.log('computer wins')
+}
+
+console.log()
