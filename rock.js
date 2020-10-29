2c113c1683b54d768d25cdb55ab5fe47d641cbe5
j-frilot <frilotdr@gmail.com>
2020-10-29 00:38:45 -0500

    added DOM components and UI

--- rock.js
+++ rock.js
@@ -2,22 +2,98 @@ let rock = "rock";
 let paper = "paper";
 let scissors = "scissors";
 
+let userRock = document.getElementById("btn-rock")
+let userPaper = document.getElementById("btn-paper")
+let userScissors = document.getElementById("btn-scissors")
 
-let choices = [`${rock}`, `${paper}`, `${scissors}`]
-let comp = choices[Math.floor(Math.random() * choices.length)];
-console.log(comp)
+userRock.addEventListener('click', playRockGame);
 
-let user = choices[Math.floor(Math.random() * choices.length)];
-console.log(user)
 
-if (user == rock && comp == scissors) {
-    console.log("user wins");
-} else if (user == paper && comp == rock) {
-    console.log("user wins");
-} else if (user == scissors && comp == paper) {
-    console.log("user wins");
-} else if (user == comp) {
-    console.log("It's a draw");
-} else {
-    console.log("comp wins")
+function playRockGame(){
+    let user = rock
+    console.log(user);
+    let choices = [`${rock}`, `${paper}`, `${scissors}`]
+    let comp = choices[Math.floor(Math.random() * choices.length)];
+    console.log(comp);
+
+    if (user == rock && comp == scissors) {
+        console.log("user wins");
+        document.getElementById("result").innerHTML="Computer chose scissors, You Won!!";
+    } else if (user == paper && comp == rock) {
+        console.log("user wins");
+        document.getElementById("result").innerHTML="Computer chose rock, You Won!!";
+    } else if (user == scissors && comp == paper) {
+        console.log("user wins");
+        document.getElementById("result").innerHTML="Computer chose paper, You Won!!";
+    } else if (user == comp) {
+        console.log("It's a draw");
+        document.getElementById("result").innerHTML="Computer chose rock, It's a tie.";
+    } else {
+        console.log("comp wins")
+        document.getElementById("result").innerHTML="Computer chose paper, You Lose";
+    }
 }
+
+
+userPaper.addEventListener('click', playPaperGame);
+
+
+function playPaperGame(){
+    let user = paper
+    console.log(user);
+    let choices = [`${rock}`, `${paper}`, `${scissors}`]
+    let comp = choices[Math.floor(Math.random() * choices.length)];
+    console.log(comp);
+
+    if (user == rock && comp == scissors) {
+        console.log("user wins");
+        document.getElementById("result").innerHTML="Computer chose scissors, You Won!!";
+    } else if (user == paper && comp == rock) {
+        console.log("user wins");
+        document.getElementById("result").innerHTML="Computer chose rock, You Won!!";
+    } else if (user == scissors && comp == paper) {
+        console.log("user wins");
+        document.getElementById("result").innerHTML="Computer chose paper, You Won!!";
+    } else if (user == comp) {
+        console.log("It's a draw");
+        document.getElementById("result").innerHTML="Computer chose paper, It's a tie.";
+    } else {
+        console.log("comp wins")
+        document.getElementById("result").innerHTML="Computer chose scissors, You Lose";
+    }
+}
+
+
+
+
+
+userScissors.addEventListener('click', playScissorsGame);
+
+
+function playScissorsGame(){
+    let user = scissors
+    console.log(user);
+    let choices = [`${rock}`, `${paper}`, `${scissors}`]
+    let comp = choices[Math.floor(Math.random() * choices.length)];
+    console.log(comp);
+
+    
+
+    if (user == rock && comp == scissors) {
+        console.log("user wins");
+        document.getElementById("result").innerHTML="Computer chose scissors, You Won!!";
+    } else if (user == paper && comp == rock) {
+        console.log("user wins");
+        document.getElementById("result").innerHTML="Computer chose rock, You Won!!";
+    } else if (user == scissors && comp == paper) {
+        console.log("user wins");
+        document.getElementById("result").innerHTML="Computer chose paper, You Won!!";
+    } else if (user == comp) {
+        console.log("It's a draw");
+        document.getElementById("result").innerHTML="Computer chose scissors, It's a tie.";
+    } else {
+        console.log("comp wins")
+        document.getElementById("result").innerHTML="Computer chose rock, You Lose";
+    }
+}
+
