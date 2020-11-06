afc5b774283ad5adad0ca49efa117a7a8eaea836
j-frilot <frilotdr@gmail.com>
2020-11-05 23:55:26 -0600

    added condition function

--- app.js/rock.js
+++ app.js/rock.js
@@ -6,45 +6,8 @@ let userRock = document.getElementById("btn-rock")
 let userPaper = document.getElementById("btn-paper")
 let userScissors = document.getElementById("btn-scissors")
 
-userRock.addEventListener('click', playRockGame);
+function condition(user, comp) {
 
-
-
-function playRockGame(){
-    let user = rock
-    console.log(user);
-    let choices = [`${rock}`, `${paper}`, `${scissors}`]
-    let comp = choices[Math.floor(Math.random() * choices.length)];
-    console.log(comp);
-    
-    if (user == rock && comp == scissors) {
-        console.log("user wins");
-        document.getElementById("result").innerHTML="Computer chose scissors, You Won!!";
-    } else if (user == paper && comp == rock) {
-        console.log("user wins");
-        document.getElementById("result").innerHTML="Computer chose rock, You Won!!";
-    } else if (user == scissors && comp == paper) {
-        console.log("user wins");
-        document.getElementById("result").innerHTML="Computer chose paper, You Won!!";
-    } else if (user == comp) {
-        console.log("It's a draw");
-        document.getElementById("result").innerHTML="Computer chose rock, It's a tie.";
-    } else {
-        console.log("comp wins")
-        document.getElementById("result").innerHTML="Computer chose paper, You Lose";
-    }
-}
-
-
-userPaper.addEventListener('click', playPaperGame);
-
-
-function playPaperGame(){
-    let user = paper
-    console.log(user);
-    let choices = [`${rock}`, `${paper}`, `${scissors}`]
-    let comp = choices[Math.floor(Math.random() * choices.length)];
-    console.log(comp);
 
     if (user == rock && comp == scissors) {
         console.log("user wins");
@@ -62,39 +25,47 @@ function playPaperGame(){
         console.log("comp wins")
         document.getElementById("result").innerHTML="Computer chose scissors, You Lose";
     }
+
+    return
 }
 
-
-
-
-
-userScissors.addEventListener('click', playScissorsGame);
-
-
-function playScissorsGame(){
-    let user = scissors
-    console.log(user);
+// ////  ROCK
+userRock.addEventListener('click', playRockGame);
+function playRockGame(){
+    let user = rock
     let choices = [`${rock}`, `${paper}`, `${scissors}`]
     let comp = choices[Math.floor(Math.random() * choices.length)];
+    
+    console.log(user);
+    console.log(comp);
+    
+    condition(rock,comp)
+}
+
+// ////  PAPER
+userPaper.addEventListener('click', playPaperGame);
+function playPaperGame(){
+    let user = paper
+    let choices = [`${rock}`, `${paper}`, `${scissors}`]
+    let comp = choices[Math.floor(Math.random() * choices.length)];
+
+    console.log(user);
     console.log(comp);
 
-    
-
-    if (user == rock && comp == scissors) {
-        console.log("user wins");
-        document.getElementById("result").innerHTML="Computer chose scissors, You Won!!";
-    } else if (user == paper && comp == rock) {
-        console.log("user wins");
-        document.getElementById("result").innerHTML="Computer chose rock, You Won!!";
-    } else if (user == scissors && comp == paper) {
-        console.log("user wins");
-        document.getElementById("result").innerHTML="Computer chose paper, You Won!!";
-    } else if (user == comp) {
-        console.log("It's a draw");
-        document.getElementById("result").innerHTML="Computer chose scissors, It's a tie.";
-    } else {
-        console.log("comp wins")
-        document.getElementById("result").innerHTML="Computer chose rock, You Lose";
-    }
+    condition(paper,comp)
 }
+
+/////  SCISSORS
+userScissors.addEventListener('click', playScissorsGame);
+function playScissorsGame(){
+    let user = scissors
+    let choices = [`${rock}`, `${paper}`, `${scissors}`]
+    let comp = choices[Math.floor(Math.random() * choices.length)];
+
+    console.log(user);
+    console.log(comp);
+
+    condition(scissors,comp)
+}
+
 
